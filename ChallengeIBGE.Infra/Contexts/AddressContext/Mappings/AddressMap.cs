a0a846fc25649acf2e26f8a073640aeb356340f9
Igor Santiago <contato.igorsantiago@outlook.com>
2023-10-16 13:10:44 -0300

    INFRA: Add Entity Mapping to Address

---
+++ ChallengeIBGE.Infra/Contexts/AddressContext/Mappings/AddressMap.cs
@@ -0,0 +1,26 @@
+﻿using ChallengeIBGE.Core.Contexts.AddressContext.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ChallengeIBGE.Infra.Contexts.AddressContext.Mappings;
+
+public class AddressMap : IEntityTypeConfiguration<Address>
+{
+    public void Configure(EntityTypeBuilder<Address> builder)
+    {
+        builder.ToTable("Address");
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Id);
+        builder.Property(x => x.State)
+            .HasColumnName("State")
+            .HasColumnType("NVARCHAR")
+            .HasMaxLength(2);
+        builder.Property(x => x.City)
+            .HasColumnName("City")
+            .HasColumnType("NVARCHAR")
+            .HasMaxLength(80);
+
+        builder.HasIndex(x => x.State).HasDatabaseName("IX_Address_State");
+        builder.HasIndex(x => x.City).HasDatabaseName("IX_Address_City");
+    }
+}
--- ChallengeIBGE.Infra/Data/DataContext.cs
+++ ChallengeIBGE.Infra/Data/DataContext.cs
@@ -1,4 +1,6 @@
-﻿using ChallengeIBGE.Core.Contexts.UserContext.Entities;
+﻿using ChallengeIBGE.Core.Contexts.AddressContext.Entities;
+using ChallengeIBGE.Core.Contexts.UserContext.Entities;
+using ChallengeIBGE.Infra.Contexts.AddressContext.Mappings;
 using ChallengeIBGE.Infra.Contexts.UserContext.Mappings;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,11 +15,13 @@ namespace ChallengeIBGE.Infra.Data
 
         public DbSet<User> Users { get; set; } = null!;
         public DbSet<Role> Roles { get; set; } = null!;
+        public DbSet<Address> Addresses { get; set; } = null!;
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfiguration(new UserMap());
             modelBuilder.ApplyConfiguration(new RoleMap());
+            modelBuilder.ApplyConfiguration(new AddressMap());
         }
     }
 }
