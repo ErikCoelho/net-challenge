5b17119961b81ca7de5a06e77d2b6b7825339a24
Igor Santiago <contato.igorsantiago@outlook.com>
2023-10-16 06:16:09 -0300

    INFRA: Add SearchUser UseCase to UserContext

---
+++ ChallengeIBGE.Infra/Contexts/UserContext/UseCases/SearchUser/Repository.cs
@@ -0,0 +1,14 @@
+﻿using ChallengeIBGE.Core.Contexts.UserContext.Entities;
+using ChallengeIBGE.Core.Contexts.UserContext.UseCases.SearchUser.Contracts;
+using ChallengeIBGE.Infra.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChallengeIBGE.Infra.Contexts.UserContext.UseCases.SearchUser;
+
+public class Repository : IRepository
+{
+    private readonly DataContext _context;
+    public Repository(DataContext context) => _context = context;
+    public async Task<User?> GetUserById(Guid id, CancellationToken cancellationToken)
+        => await _context.Users.FirstOrDefaultAsync(x => x.Id == id, cancellationToken);
+}
