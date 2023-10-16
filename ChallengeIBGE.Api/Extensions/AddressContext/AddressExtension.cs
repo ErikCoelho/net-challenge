2cd9cc1a332788f4697c4ceef9a90b45a0e55de3
Igor Santiago <contato.igorsantiago@outlook.com>
2023-10-16 06:42:53 -0300

    API: Create structure to AddressContext and UserContext endpoints

---
+++ ChallengeIBGE.Api/Extensions/AddressContext/AddressExtension.cs
@@ -0,0 +1,81 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChallengeIBGE.Api.Extensions.AddressContext;
+
+public static class AddressExtension
+{
+    public static void AddAddressContext(this WebApplicationBuilder builder)
+    {
+        #region CreateAddress
+        builder.Services.AddTransient<
+            Core.Contexts.AddressContext.UseCases.CreateAddress.Contracts.IRepository,
+            Infra.Contexts.AddressContext.UseCases.Create.Repository>();
+        #endregion
+
+        #region DeleteAddress
+        builder.Services.AddTransient<
+            Core.Contexts.AddressContext.UseCases.DeleteAddress.Contracts.IRepository,
+            Infra.Contexts.AddressContext.UseCases.Delete.Repository>();
+        #endregion
+
+        #region ListAddresses
+        builder.Services.AddTransient<
+            Core.Contexts.AddressContext.UseCases.ListAddresses.Contracts.IRepository,
+            Infra.Contexts.AddressContext.UseCases.List.Repository>();
+        #endregion
+
+        #region UpdateAddress
+        builder.Services.AddTransient<
+            Core.Contexts.AddressContext.UseCases.UpdateAddress.Contracts.IRepository,
+            Infra.Contexts.AddressContext.UseCases.Update.Repository>();
+        #endregion
+    }
+
+    public static void MapAddressEndpoints(this WebApplication app)
+    {
+        #region CreateAddress
+        app.MapPost("api/v1/address/create", async (
+            [FromBody] Core.Contexts.AddressContext.UseCases.CreateAddress.Request request,
+            [FromServices] IRequestHandler<
+                Core.Contexts.AddressContext.UseCases.CreateAddress.Request,
+                Core.Contexts.AddressContext.UseCases.CreateAddress.Response> handler) =>
+        {
+
+        });
+        #endregion
+
+        #region DeleteAddress
+        app.MapDelete("api/v1/address/delete", async (
+            [FromBody] Core.Contexts.AddressContext.UseCases.DeleteAddress.Request request,
+            [FromServices] IRequestHandler<
+                Core.Contexts.AddressContext.UseCases.DeleteAddress.Request,
+                Core.Contexts.AddressContext.UseCases.DeleteAddress.Response> handler) =>
+        {
+
+        });
+        #endregion
+
+        #region ListAddresses
+        app.MapGet("api/v1/address/search", async (
+            [FromBody] Core.Contexts.AddressContext.UseCases.ListAddresses.Request request,
+            [FromServices] IRequestHandler<
+                Core.Contexts.AddressContext.UseCases.ListAddresses.Request,
+                Core.Contexts.AddressContext.UseCases.ListAddresses.Response> handler) =>
+        {
+
+        });
+        #endregion
+
+        #region UpdateAddress
+        app.MapPut("api/v1/address/update", async (
+            [FromBody] Core.Contexts.AddressContext.UseCases.UpdateAddress.Request request,
+            [FromServices] IRequestHandler<
+                Core.Contexts.AddressContext.UseCases.UpdateAddress.Request,
+                Core.Contexts.AddressContext.UseCases.UpdateAddress.Response> handler) =>
+        {
+
+        });
+        #endregion
+    }
+}
---
+++ ChallengeIBGE.Api/Extensions/UserContextExtensions/UserExtension.cs
@@ -0,0 +1,98 @@
+﻿namespace ChallengeIBGE.Api.Extensions.UserContextExtensions;
+
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+public static class UserExtension
+{
+    public static void AddUserContext(this WebApplicationBuilder builder)
+    {
+        #region Authenticate
+        builder.Services.AddTransient<
+            Core.Contexts.UserContext.UseCases.Authenticate.Contracts.IRepository,
+            Infra.Contexts.UserContext.UseCases.Authenticate.Repository>();
+        #endregion
+
+        #region CreateUser
+        builder.Services.AddTransient<
+            Core.Contexts.UserContext.UseCases.CreateUser.Contracts.IRepository,
+            Infra.Contexts.UserContext.UseCases.Create.Repository>();
+        #endregion
+
+        #region DeleteUser
+        builder.Services.AddTransient<
+            Core.Contexts.UserContext.UseCases.DeleteUser.Contracts.IRepository,
+            Infra.Contexts.UserContext.UseCases.Delete.Repository>();
+        #endregion
+
+        #region SearchUser
+        builder.Services.AddTransient<
+            Core.Contexts.UserContext.UseCases.SearchUser.Contracts.IRepository,
+            Infra.Contexts.UserContext.UseCases.SearchUser.Repository>();
+        #endregion
+
+        #region UpdateUser
+        builder.Services.AddTransient<
+            Core.Contexts.UserContext.UseCases.UpdateUser.Contracts.IRepository,
+            Infra.Contexts.UserContext.UseCases.Update.Repository>();
+        #endregion
+    }
+
+    public static void MapUserEndpoints(this WebApplication app)
+    {
+        #region Authenticate
+        app.MapPost("api/v1/authenticate", async (
+            [FromBody] Core.Contexts.UserContext.UseCases.Authenticate.Request request,
+            [FromServices] IRequestHandler<
+                Core.Contexts.UserContext.UseCases.Authenticate.Request,
+                Core.Contexts.UserContext.UseCases.Authenticate.Response> handler) =>
+        {
+
+        });
+        #endregion
+
+        #region CreateUser
+        app.MapPost("api/v1/user/create", async (
+            [FromBody] Core.Contexts.UserContext.UseCases.CreateUser.Request request,
+            [FromServices] IRequestHandler<
+                Core.Contexts.UserContext.UseCases.CreateUser.Request,
+                Core.Contexts.UserContext.UseCases.CreateUser.Response> handler) =>
+        {
+
+        });
+        #endregion
+
+        #region DeleteUser
+        app.MapDelete("api/v1/user/delete", async (
+            [FromBody] Core.Contexts.UserContext.UseCases.DeleteUser.Request request,
+            [FromServices] IRequestHandler<
+                Core.Contexts.UserContext.UseCases.DeleteUser.Request,
+                Core.Contexts.UserContext.UseCases.DeleteUser.Response> handler) =>
+        {
+
+        });
+        #endregion
+
+        #region SearchUser
+        app.MapGet("api/v1/user/search", async (
+            [FromBody] Core.Contexts.UserContext.UseCases.SearchUser.Request request,
+            [FromServices] IRequestHandler<
+                Core.Contexts.UserContext.UseCases.SearchUser.Request,
+                Core.Contexts.UserContext.UseCases.SearchUser.Response> handler) =>
+        {
+
+        });
+        #endregion
+
+        #region UpdateUser
+        app.MapPut("api/v1/user/update", async (
+            [FromBody] Core.Contexts.UserContext.UseCases.UpdateUser.Request request,
+            [FromServices] IRequestHandler<
+                Core.Contexts.UserContext.UseCases.UpdateUser.Request,
+                Core.Contexts.UserContext.UseCases.UpdateUser.Response> handler) =>
+        {
+
+        });
+        #endregion
+    }
+}
